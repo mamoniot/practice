b7fd6cad3d88bde8af3ffee64d04ea29296795f4
Monica Moniot <mamoniot@protonmail.com>
2023-07-21 13:05:37 -0400

    saving

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,29 @@
-mod pool;
 
-use pool::*;
 
-struct Test (usize, usize);
-impl Drop for Test {
-	fn drop(&mut self) {
-		println!("drop {}", self.0);
-	}
+
+pub enum Op {
+	A,
+	B,
+	C,
+}
+
+fn add_op(queue: &[Op], op: Op) {
+
+}
+fn process_ops(queue: &[Op]) {
+
 }
 
 fn main() {
-	const N: usize = 10;
-	let pool = Pool::<_, 100>::with_capacity();
-	let mut v: [_; N] = std::array::from_fn(|i| pool.alloc_raii(Test(i, i)));
-
-	for j in 0..N {
-		let i = (j*13)%N;
-		let p = pool.alloc_raii(Test(v[i].0 + N, i));
-		println!("add  {}", p.0);
-		v[i] = p;
+	match 1 {
+		1 => {
+			println!("1")
+		}
+		| 2 => {
+			println!("2")
+		}
+		_ => {
+			println!("x")
+		}
 	}
 }
